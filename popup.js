683138f6bc3655926eafe86a19ebbbe25625416d
Leah Mattingly <lmattingly6@ivytech.edu>
2024-01-05 20:39:06 -0600

    creaating an instance of a popup. The popup informs user that the webpage is a demo site.

---
+++ popup.js
@@ -0,0 +1,10 @@
+// popup.js
+
+// This function will be executed when the page loads
+function showAlert() {
+    alert("This is a Demo site designed by Leah Mattingly");
+  }
+  
+  // Attach the showAlert function to the window.onload event
+  window.onload = showAlert;
+  
